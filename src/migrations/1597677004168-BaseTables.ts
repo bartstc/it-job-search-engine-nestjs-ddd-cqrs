d68d8b21f0a2dc9a5334001e4c1b66ba71d02c09
bartstc <bartst010395@gmail.com>
2020-08-17 17:51:03 +0200

    Add docker compose file

--- src/migrations/1597677004168-BaseTables.ts
+++ src/migrations/1597677004168-BaseTables.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateBaseTables1597666192692 implements MigrationInterface {
-    name = 'CreateBaseTables1597666192692'
+export class BaseTables1597677004168 implements MigrationInterface {
+    name = 'BaseTables1597677004168'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "offer" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "offerId" character varying NOT NULL, "title" character varying NOT NULL, "description" character varying NOT NULL, "companyName" character varying NOT NULL, "technology" character varying NOT NULL, "level" character varying NOT NULL, "employmentType" character varying NOT NULL, "cityName" character varying NOT NULL, "streetName" character varying NOT NULL, "priceMin" integer NOT NULL, "priceMax" integer NOT NULL, "currency" character varying NOT NULL, "longitude" numeric NOT NULL, "latitude" numeric NOT NULL, "mustHave" text array NOT NULL, "niceToHave" text array NOT NULL, CONSTRAINT "PK_62a0cf767cb4e012b0b4547f5ac" PRIMARY KEY ("offerId"))`);
+        await queryRunner.query(`CREATE TABLE "offers" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "offerId" character varying NOT NULL, "title" character varying NOT NULL, "description" character varying NOT NULL, "companyName" character varying NOT NULL, "technology" character varying NOT NULL, "level" character varying NOT NULL, "employmentType" character varying NOT NULL, "cityName" character varying NOT NULL, "streetName" character varying NOT NULL, "priceMin" integer NOT NULL, "priceMax" integer NOT NULL, "currency" character varying NOT NULL, "longitude" numeric NOT NULL, "latitude" numeric NOT NULL, "mustHave" text array NOT NULL, "niceToHave" text array NOT NULL, CONSTRAINT "PK_7016eccf0b5dc977f248f1841f6" PRIMARY KEY ("offerId"))`);
         await queryRunner.query(`CREATE TABLE "roles" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "roleId" character varying NOT NULL, "name" character varying NOT NULL, "contextType" character varying NOT NULL, "permissions" text array NOT NULL, CONSTRAINT "PK_39bf7e8af8fe54d9d1c7a8efe6f" PRIMARY KEY ("roleId"))`);
         await queryRunner.query(`CREATE TABLE "users" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" character varying NOT NULL, "email" character varying NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "isDeleted" boolean NOT NULL DEFAULT false, "isEmailVerified" boolean NOT NULL DEFAULT false, "contextType" character varying NOT NULL, "roleIds" text array NOT NULL, CONSTRAINT "UQ_772886e2f1f47b9ceb04a06e203" UNIQUE ("email", "username"), CONSTRAINT "PK_8bf09ba754322ab9c22a215c919" PRIMARY KEY ("userId"))`);
     }
@@ -12,7 +12,7 @@ export class CreateBaseTables1597666192692 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "users"`);
         await queryRunner.query(`DROP TABLE "roles"`);
-        await queryRunner.query(`DROP TABLE "offer"`);
+        await queryRunner.query(`DROP TABLE "offers"`);
     }
 
 }
--- src/modules/offers/entities/offer.entity.ts
+++ src/modules/offers/entities/offer.entity.ts
@@ -3,7 +3,11 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { AbstractEntity } from 'shared/core';
 
 import { Currency, EmploymentType, Level, Technology } from '../domain/types';
-
+const test = `INSERT INTO offers (offerId, title, description, companyName, technology, level, employmentType, cityName, streetName, priceMin, priceMax, currency, longitude, latitude)
+            VALUES ('ace36259-eb62-4c1e-a0eb-75b7de29d012', 'React Developer', 'test', 'Madkom', 'javascript', 'junior', 'b2b', 'Gdynia', 'Al. Zwycięstwa', '2000', '4000', 'PLN', '90.1234', '120.2234'),
+            ('ace36259-eb62-4c1e-a0eb-75b7de29d013', 'React Developer', 'test', 'Madkom', 'javascript', 'junior', 'b2b', 'Gdynia', 'Al. Zwycięstwa', '2000', '4000', 'PLN', '90.1234', '120.2234'),
+            ('ace36259-eb62-4c1e-a0eb-75b7de29d014', 'React Developer', 'test', 'Madkom', 'javascript', 'junior', 'b2b', 'Gdynia', 'Al. Zwycięstwa', '2000', '4000', 'PLN', '90.1234', '120.2234')
+        `
 @Entity('offers')
 export class OfferEntity extends AbstractEntity {
   @PrimaryColumn()
